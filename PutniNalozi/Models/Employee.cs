9fed66d0e3ca74dff3d61f3511409f1b0af80c22
v-filipovic <drug.filip@gmail.com>
2015-04-16 15:27:28 +0200

    Initial commit

---
+++ PutniNalozi/Models/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PutniNalozi.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "Ime")]
+        [Required]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Prezime")]
+        [Required]
+        public string LastName { get; set; }
+    }
+
+    public class EmployeeDBContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ PutniNalozi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PutniNalozi.Startup))]
+namespace PutniNalozi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
